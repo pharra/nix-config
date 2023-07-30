79d947ee63670b04c753075f8ca6f59becd09929
pharra <typechasing@gmail.com>
2023-07-30 22:55:07 +0800

    restore ssh config

--- home/desktop/ssh.nix
+++ home/desktop/ssh.nix
@@ -9,39 +9,27 @@
     #   IdentityFile — the location of your SSH key authentication file for the account.
     # format in details:
     #   https://www.ssh.com/academy/ssh/config
-    extraConfig = ''
-      # a private key that is used during authentication will be added to ssh-agent if it is running
-      AddKeysToAgent yes
+    # extraConfig = ''
+    #   # a private key that is used during authentication will be added to ssh-agent if it is running
+    #   AddKeysToAgent yes
 
-      Host 192.168.*
-        # allow to securely use local SSH agent to authenticate on the remote machine.
-        # It has the same effect as adding cli option `ssh -A user@host`
-        ForwardAgent yes
-        # romantic holds my homelab~
-        IdentityFile ~/.ssh/romantic
-        # Specifies that ssh should only use the identity file explicitly configured above
-        # required to prevent sending default identity files first.
-        IdentitiesOnly yes
+    #   Host 192.168.*
+    #     # allow to securely use local SSH agent to authenticate on the remote machine.
+    #     # It has the same effect as adding cli option `ssh -A user@host`
+    #     ForwardAgent yes
+    #     # romantic holds my homelab~
+    #     IdentityFile ~/.ssh/romantic
+    #     # Specifies that ssh should only use the identity file explicitly configured above
+    #     # required to prevent sending default identity files first.
+    #     IdentitiesOnly yes
 
-      Host github.com
-          # github is controlled by gluttony~
-          IdentityFile ~/.ssh/gluttony
-          # Specifies that ssh should only use the identity file explicitly configured above
-          # required to prevent sending default identity files first.
-          IdentitiesOnly yes
-
-      Host gtr5
-        HostName 192.168.5.172
-        Port 22
-
-      Host um560
-        HostName 192.168.5.173
-        Port 22
-
-      Host s500plus
-        HostName 192.168.5.174
-        Port 22
-    '';
+    #   Host github.com
+    #       # github is controlled by gluttony~
+    #       IdentityFile ~/.ssh/gluttony
+    #       # Specifies that ssh should only use the identity file explicitly configured above
+    #       # required to prevent sending default identity files first.
+    #       IdentitiesOnly yes
+    # '';
 
     # use ssh-agent so we only need to input passphrase once
     # run `ssh-add /path/to/key` for every identity file
