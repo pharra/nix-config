112bc188de27109c510839f5a567e7b338132957
pharra <typechasing@gmail.com>
2023-09-15 11:57:48 +0800

    remove dpdk depends

--- pkgs/spdk/default.nix
+++ pkgs/spdk/default.nix
@@ -7,7 +7,6 @@
   ncurses,
   python3,
   cunit,
-  dpdk,
   libaio,
   libbsd,
   libuuid,
@@ -28,90 +27,90 @@
   cmocka,
   meson,
   ninja,
-}: let
-  python3' = python3.withPackages (ps: [
-    ps.configshell
-  ]);
-in
-  stdenv.mkDerivation rec {
-    pname = "spdk";
-    version = "23.05";
+  rdma-core,
+}:
+stdenv.mkDerivation rec {
+  pname = "spdk";
+  version = "23.05";
 
-    src = fetchgit {
-      url = "https://github.com/spdk/spdk.git";
-      rev = "a1f4f11b41e4fa9fe9242fa8467a3eee92c20a03";
-      fetchSubmodules = true;
-      sha256 = "sha256-nfHW/oXEzSQ4FmXervDG5+E6OP40MWiXy17Ey1GAehQ=";
-    };
+  src = fetchgit {
+    url = "https://github.com/spdk/spdk.git";
+    rev = "a1f4f11b41e4fa9fe9242fa8467a3eee92c20a03";
+    fetchSubmodules = true;
+    sha256 = "sha256-nfHW/oXEzSQ4FmXervDG5+E6OP40MWiXy17Ey1GAehQ=";
+  };
 
-    patches = [
-      ./python-setup.patch
-    ];
+  patches = [
+    ./python-setup.patch
+  ];
 
-    nativeBuildInputs = [
-      python3'
-    ];
+  nativeBuildInputs = [
+    python3
+    python3.pkgs.configshell
+    python3.pkgs.sphinx
+    python3.pkgs.pyelftools
+  ];
 
-    buildInputs = [
-      cunit
-      dpdk
-      libaio
-      libbsd
-      libuuid
-      numactl
-      openssl
-      ncurses
-      pkg-config
-      zlib
-      libpcap
-      libnl
-      libelf
-      jansson
-      nasm
-      autoconf269
-      automake
-      libtool
-      liburing
-      json_c
-      cmocka
-      meson
-      ninja
-    ];
+  buildInputs = [
+    cunit
+    libaio
+    libbsd
+    libuuid
+    numactl
+    openssl
+    ncurses
+    pkg-config
+    zlib
+    libpcap
+    libnl
+    libelf
+    jansson
+    nasm
+    autoconf269
+    automake
+    libtool
+    liburing
+    json_c
+    cmocka
+    meson
+    ninja
+    rdma-core
+  ];
 
-    preConfigure = ''
-      export AS=nasm
-    '';
+  preConfigure = ''
+    export AS=nasm
+  '';
 
-    postPatch = ''
-      patchShebangs .
+  postPatch = ''
+    patchShebangs .
 
-      # glibc-2.36 adds arc4random, so we don't need the custom implementation
-      # here anymore. Fixed upstream in https://github.com/spdk/spdk/commit/43a3984c6c8fde7201d6c8dfe1b680cb88237269,
-      # but the patch doesn't apply here.
-      sed -i -e '1i #define HAVE_ARC4RANDOM 1' lib/iscsi/iscsi.c
-    '';
+    # glibc-2.36 adds arc4random, so we don't need the custom implementation
+    # here anymore. Fixed upstream in https://github.com/spdk/spdk/commit/43a3984c6c8fde7201d6c8dfe1b680cb88237269,
+    # but the patch doesn't apply here.
+    sed -i -e '1i #define HAVE_ARC4RANDOM 1' lib/iscsi/iscsi.c
+  '';
 
-    dontUseMesonConfigure = true;
-    enableParallelBuilding = true;
-    dontUseNinjaBuild = true;
-    dontUseNinjaInstall = true;
+  dontUseMesonConfigure = true;
+  enableParallelBuilding = true;
+  dontUseNinjaBuild = true;
+  dontUseNinjaInstall = true;
 
-    configureFlags = ["--with-dpdk=${dpdk}" "--with-rdma" "--with-uring" "--with-ublk" "--with-vfio-user"];
+  configureFlags = ["--with-rdma" "--with-uring" "--with-ublk" "--with-vfio-user"];
 
-    postInstall = ''
-      cp -r scripts $out
-      cp -r python/spdk $out/scripts
-    '';
+  postInstall = ''
+    cp -r scripts $out
+    cp -r python/spdk $out/scripts
+  '';
 
-    env.NIX_CFLAGS_COMPILE = "-mssse3"; # Necessary to compile.
-    # otherwise does not find strncpy when compiling
-    NIX_LDFLAGS = "-lbsd";
+  env.NIX_CFLAGS_COMPILE = "-mssse3"; # Necessary to compile.
+  # otherwise does not find strncpy when compiling
+  NIX_LDFLAGS = "-lbsd";
 
-    meta = with lib; {
-      description = "Set of libraries for fast user-mode storage";
-      homepage = "https://spdk.io/";
-      license = licenses.bsd3;
-      platforms = ["x86_64-linux"];
-      maintainers = with maintainers; [orivej];
-    };
-  }
+  meta = with lib; {
+    description = "Set of libraries for fast user-mode storage";
+    homepage = "https://spdk.io/";
+    license = licenses.bsd3;
+    platforms = ["x86_64-linux"];
+    maintainers = with maintainers; [orivej];
+  };
+}
