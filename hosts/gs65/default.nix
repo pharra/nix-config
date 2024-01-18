feba687568b48d237c6b949ccaa9d6e82c5c891c
pharra <typechasing@gmail.com>
2024-01-18 09:09:42 +0800

    update gs65 hostname

--- hosts/gs65/default.nix
+++ hosts/gs65/default.nix
@@ -51,7 +51,7 @@
   };
 
   networking = {
-    hostName = "desktop";
+    hostName = "gs65";
     wireless.enable = false; # Enables wireless support via wpa_supplicant.
 
     # Configure network proxy if necessary
