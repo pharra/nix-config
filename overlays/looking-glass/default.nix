194d7aec5c349b694860219716e4e41f46c447e0
pharra <typechasing@gmail.com>
2024-01-19 21:41:22 +0800

    update looking glass client version

--- overlays/looking-glass/default.nix
+++ overlays/looking-glass/default.nix
@@ -1,9 +1,10 @@
 self: super: {
   looking-glass-client = super.looking-glass-client.overrideAttrs (oldAttrs: rec {
+    version = "c2237f29";
     src = super.fetchFromGitHub {
       owner = "gnif";
       repo = "LookingGlass";
-      rev = "e658c2e0a205c40701b00d97364c2a9903ed34cf";
+      rev = "c2237f29ae891316b32a534e5d0dfec6616d6294";
       sha256 = "sha256-AOb79RiHpYnrPv/jHCijAgr4uIe+TUIsY8pmVt0b0cU=";
       fetchSubmodules = true;
     };
