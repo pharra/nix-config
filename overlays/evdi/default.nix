d56a0022b09b5505c7cc2a636d151424c657aa23
pharra <typechasing@gmail.com>
2024-01-19 19:12:54 +0800

    update evdi

--- overlays/evdi/default.nix
+++ overlays/evdi/default.nix
@@ -1,12 +1,14 @@
 self: super: {
-  evdi = super.evdi.overrideAttrs (oldAttrs: rec {
-    version = "unstable-2024-01-08";
-    src = super.fetchFromGitHub {
-      owner = "DisplayLink";
-      repo = "evdi";
-      rev = "0313ecac7aa3990c6f9a0d0f258c87e20e116bdd";
-      hash = "sha256-OebHunR67QUrh8GKTuxb8WI6+/M/y/nVhQME+1An3JA=";
-    };
-    meta.broken = super.kernel.kernelOlder "4.19";
+  linux_mlx = super.linux_mlx.extend  (lpself: lpsuper: {
+    evdi = super.linux_mlx.evdi.overrideAttrs (oldAttrs: {
+      version = "unstable-2024-01-08";
+      src = super.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "0313ecac7aa3990c6f9a0d0f258c87e20e116bdd";
+        hash = "sha256-OebHunR67QUrh8GKTuxb8WI6+/M/y/nVhQME+1An3JA=";
+      };
+      meta.broken = lpsuper.kernel.kernelOlder "4.19";
+    });
   });
 }
