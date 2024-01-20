699c7093e0b03761008ed86ddda70f9c27a85b2c
pharra <typechasing@gmail.com>
2024-01-20 15:05:49 +0800

    update looking glass client version

--- overlays/looking-glass/default.nix
+++ overlays/looking-glass/default.nix
@@ -1,12 +1,16 @@
 self: super: {
   looking-glass-client = super.looking-glass-client.overrideAttrs (oldAttrs: rec {
-    version = "c2237f29";
+    version = "B6-234-c2237f29";
     src = super.fetchFromGitHub {
       owner = "gnif";
       repo = "LookingGlass";
       rev = "c2237f29ae891316b32a534e5d0dfec6616d6294";
-      sha256 = "sha256-AOb79RiHpYnrPv/jHCijAgr4uIe+TUIsY8pmVt0b0cU=";
+      sha256 = "sha256-5arzCjaRrR6wAcSoKLDzOil56gQTw2YXvbXKmkcipAY=";
       fetchSubmodules = true;
     };
+    postUnpack = ''
+      echo ${version} > source/VERSION
+      export sourceRoot="source/client"
+    '';
   });
 }
