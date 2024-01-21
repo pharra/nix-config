95fe3a648560937a95be34031771f084a1bbef27
pharra <typechasing@gmail.com>
2024-01-21 21:24:11 +0800

    disable nfs mount

--- hosts/desktop/cifs-mount.nix
+++ hosts/desktop/cifs-mount.nix
@@ -5,25 +5,25 @@
   ...
 }: {
   # mount a smb/cifs share
-  services.rpcbind.enable = true; # needed for NFS
-  systemd.mounts = [
-    {
-      type = "nfs";
-      mountConfig = {
-        Options = "noatime";
-      };
-      what = "homelab.intern:/nfs/persistent";
-      where = "/nix-persistent";
-    }
-  ];
+  # services.rpcbind.enable = true; # needed for NFS
+  # systemd.mounts = [
+  #   {
+  #     type = "nfs";
+  #     mountConfig = {
+  #       Options = "noatime";
+  #     };
+  #     what = "homelab.intern:/nfs/persistent";
+  #     where = "/nix-persistent";
+  #   }
+  # ];
 
-  systemd.automounts = [
-    {
-      wantedBy = ["multi-user.target"];
-      automountConfig = {
-        TimeoutIdleSec = "600";
-      };
-      where = "/nix-persistent";
-    }
-  ];
+  # systemd.automounts = [
+  #   {
+  #     wantedBy = ["multi-user.target"];
+  #     automountConfig = {
+  #       TimeoutIdleSec = "600";
+  #     };
+  #     where = "/nix-persistent";
+  #   }
+  # ];
 }
