b252d5525e2ec1db0f7d0e3ca3709ed060147c82
pharra <typechasing@gmail.com>
2024-01-21 18:46:17 +0800

    update looking glass client config

--- home/desktop/looking-glass.nix
+++ home/desktop/looking-glass.nix
@@ -6,6 +6,8 @@
 }: {
   xdg.configFile."looking-glass/client.ini".text = lib.generators.toINI {} {
     app.shmFile = "/dev/kvmfr0";
+    app.cursorPollInterval = 1;
+    app.framePollInterval = 1;
     input.escapeKey = 119; # key pause
     input.rawMouse = "yes";
     spice.enable = "yes";
