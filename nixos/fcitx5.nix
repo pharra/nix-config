e463ef1386402706e96eebd40a9ab666cf7b7bd2
pharra <typechasing@gmail.com>
2024-03-03 22:25:57 +0800

    fix fcitx5 in wayland

--- nixos/fcitx5.nix
+++ nixos/fcitx5.nix
@@ -8,6 +8,7 @@
 } @ args: {
   i18n.inputMethod = {
     enabled = "fcitx5";
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
       fcitx5-gtk
@@ -19,12 +20,18 @@
 
   # Extra variables not covered by NixOS fcitx module
   environment.variables = {
-    INPUT_METHOD = "fcitx";
-    SDL_IM_MODULE = "fcitx";
-    GLFW_IM_MODULE = "ibus";
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
+    # INPUT_METHOD = "fcitx";
+    # SDL_IM_MODULE = "fcitx";
+    # GLFW_IM_MODULE = "ibus";
+    # GTK_IM_MODULE = "fcitx";
+    # QT_IM_MODULE = "fcitx";
+    # XMODIFIERS = "@im=fcitx";
+    # IMSETTINGS_MODULE = "fcitx";
+
+    # GTK_IM_MODULE = "wayland";
+    # QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
-    IMSETTINGS_MODULE = "fcitx";
+    # SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
   };
 }
