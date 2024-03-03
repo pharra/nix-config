503b17b6c90418e81cbccd4e29a7bbc50753bde5
pharra <typechasing@gmail.com>
2024-03-03 20:45:33 +0800

    update kde to plasma6

--- nixos/kde.nix
+++ nixos/kde.nix
@@ -17,7 +17,8 @@
 
       displayManager.sddm.enable = true; # Display Manager
       #displayManager.sddm.autoLogin.relogin = true;
-      desktopManager.plasma5.enable = true; # Window Manager
+      displayManager.sddm.wayland.enable = true;
+      desktopManager.plasma6.enable = true; # Window Manager
       displayManager.autoLogin = {
         enable = true;
         user = "${username}";
