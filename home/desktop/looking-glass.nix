3d7fe603594f2c2eb366f82224310f25811daf52
pharra <typechasing@gmail.com>
2024-03-16 01:10:27 +0800

    looking-glass-client: update config

--- home/desktop/looking-glass.nix
+++ home/desktop/looking-glass.nix
@@ -7,13 +7,13 @@
   xdg.configFile."looking-glass/client.ini".text = lib.generators.toINI {} {
     app.shmFile = "/dev/kvmfr0";
     input.escapeKey = 119; # key pause
-    input.rawMouse = "yes";
-    spice.enable = "yes";
-    win.autoScreensaver = "yes";
-    win.fullScreen = "yes";
-    win.jitRender = "yes";
-    wayland.fractionScale = "no";
-    wayland.warpSupport = "no";
+    # input.rawMouse = "yes";
+    # spice.enable = "yes";
+    # win.autoScreensaver = "yes";
+    # win.fullScreen = "yes";
+    # win.jitRender = "yes";
+    # wayland.fractionScale = "no";
+    # wayland.warpSupport = "no";
     # win.quickSplash = "yes";
     audio.micDefault = "allow";
   };
