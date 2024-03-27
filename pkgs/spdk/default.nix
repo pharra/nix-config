020cc2d5b46f118ea79f1f23f2619ee16df96e81
pharra <typechasing@gmail.com>
2024-03-27 20:29:07 +0800

    spdk: update to version 24.01

--- pkgs/spdk/default.nix
+++ pkgs/spdk/default.nix
@@ -29,22 +29,21 @@
   ninja,
   rdma-core,
   ensureNewerSourcesForZipFilesHook,
+  fuse3,
+  fetchFromGitHub,
+  autoPatchelfHook,
 }:
 stdenv.mkDerivation rec {
   pname = "spdk";
-  version = "23.09";
+  version = "24.01";
 
-  src = fetchgit {
-    url = "https://github.com/spdk/spdk.git";
-    rev = "726c8313fd559711a456cae2c335638f31aa79a6";
+  src = fetchFromGitHub {
+    owner = "spdk";
+    repo = "spdk";
+    rev = "v${version}";
+    sha256 = "sha256-5znYELR6WvVXbfFKAcRtJnSwAE5WHmA8v1rvZUtszS4=";
     fetchSubmodules = true;
-    sha256 = "sha256-elwOfO2Bg3r8FRy1fwKAOoPdRdqhd9CjDkmlFNRGr5w=";
   };
-
-  patches = [
-    ./setuptools.patch
-    ./0001-fix-setuptools-installation.patch
-  ];
 
   nativeBuildInputs = [
     python3
@@ -52,8 +51,12 @@ stdenv.mkDerivation rec {
     python3.pkgs.sphinx
     python3.pkgs.pyelftools
     python3.pkgs.setuptools
+    python3.pkgs.pip
+    python3.pkgs.wheel
+    python3.pkgs.wrapPython
     pkg-config
     ensureNewerSourcesForZipFilesHook
+    autoPatchelfHook
   ];
 
   buildInputs = [
@@ -80,11 +83,16 @@ stdenv.mkDerivation rec {
     meson
     ninja
     rdma-core
+    fuse3
   ];
 
   preConfigure = ''
     export AS=nasm
   '';
+
+  propagatedBuildInputs = [
+    python3.pkgs.configshell
+  ];
 
   postPatch = ''
     patchShebangs .
@@ -95,13 +103,17 @@ stdenv.mkDerivation rec {
   dontUseNinjaBuild = true;
   dontUseNinjaInstall = true;
 
-  configureFlags = ["--with-rdma" "--with-uring" "--with-ublk" "--with-vfio-user" "--with-raid5f" "--pydir=${placeholder "out"}"];
+  configureFlags = ["--with-rdma" "--with-uring" "--with-ublk" "--with-vfio-user" "--with-raid5f"];
 
   postInstall = ''
     cp -r scripts $out
   '';
   postCheck = ''
     python3 -m spdk
+  '';
+
+  postFixup = ''
+    wrapPythonPrograms
   '';
 
   env.NIX_CFLAGS_COMPILE = "-mssse3"; # Necessary to compile.
