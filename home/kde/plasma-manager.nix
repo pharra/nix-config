461e382d8494e80061d320b2d3e9a3a5a31a1f03
pharra <typechasing@gmail.com>
2024-04-05 20:48:10 +0800

    plasma6: update plasma manager config

--- home/kde/plasma-manager.nix
+++ home/kde/plasma-manager.nix
@@ -5,14 +5,14 @@
       "org_kde_powerdevil"."Turn Off Screen" = [];
     };
     configFile = {
-      "kdeglobals"."KDE"."SingleClick" = false;
-      "kwinrc"."Wayland"."InputMethod[$e]" = "/run/current-system/sw/share/applications/org.fcitx.Fcitx5.desktop";
-      "kwinrc"."Xwayland"."Scale" = 2;
-      "kxkbrc"."Layout"."DisplayNames" = "";
-      "kxkbrc"."Layout"."LayoutList" = "us";
-      "kxkbrc"."Layout"."Use" = true;
-      "kxkbrc"."Layout"."VariantList" = "";
-      "plasma-localerc"."Formats"."LANG" = "en_US.UTF-8";
+      "kdeglobals"."KDE"."SingleClick".value = false;
+      "kwinrc"."Wayland"."InputMethod[$e]".value = "/run/current-system/sw/share/applications/org.fcitx.Fcitx5.desktop";
+      "kwinrc"."Xwayland"."Scale".value = 2;
+      "kxkbrc"."Layout"."DisplayNames".value = "";
+      "kxkbrc"."Layout"."LayoutList".value = "us";
+      "kxkbrc"."Layout"."Use".value = true;
+      "kxkbrc"."Layout"."VariantList".value = "";
+      "plasma-localerc"."Formats"."LANG".value = "en_US.UTF-8";
     };
   };
 }
