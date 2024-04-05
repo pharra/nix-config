c596745654088c484876c2c35d776b442f61043d
pharra <typechasing@gmail.com>
2024-04-05 20:48:27 +0800

    flatpak: remove fix-fonts-icons.patch

--- overlays/flatpak/default.nix
+++ overlays/flatpak/default.nix
@@ -1,11 +1,5 @@
 self: super: {
   flatpak = super.flatpak.overrideAttrs (oldAttrs: rec {
-    patches =
-      oldAttrs.patches
-      ++ [
-        ./fix-fonts-icons.patch
-      ];
-
     buildInputs = oldAttrs.buildInputs ++ [super.makeWrapper];
     postInstall = (oldAttrs.postInstall or "") + "wrapProgram $out/bin/flatpak --set LANGUAGE zh_CN";
   });
