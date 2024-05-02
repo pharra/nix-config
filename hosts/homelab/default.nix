4ea420304c4f6cdc3d1caf2d3e8d56b55c505e2e
pharra <typechasing@gmail.com>
2024-05-03 00:20:51 +0800

    homelab: allow ipv6 forward

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -100,6 +100,8 @@ in {
     enable = true;
   };
 
+  boot.kernel.sysctl."net.ipv4.ip_forward" = "1";
+  boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = "1";
   systemd.network = {
     enable = true;
     wait-online.anyInterface = true;
