9c3492afda538aeac0f3936c7018179a6b17ab32
pharra <typechasing@gmail.com>
2024-05-19 20:59:10 +0800

    homelab_desktop: disable firewall

--- hosts/homelab_desktop/default.nix
+++ hosts/homelab_desktop/default.nix
@@ -1,4 +1,8 @@
-{config, ...} @ args:
+{
+  config,
+  lib,
+  ...
+} @ args:
 #############################################################
 #
 #  Ai - my main computer, with NixOS + I5-13600KF + RTX 4090 GPU, for gaming & daily use.
