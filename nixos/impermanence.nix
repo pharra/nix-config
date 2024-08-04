d218245e1ab8bdf52b44b65b9d9a3b6a373056a8
pharra <typechasing@gmail.com>
2024-08-04 19:58:30 +0800

    homelab: root :update

--- nixos/impermanence.nix
+++ nixos/impermanence.nix
@@ -74,11 +74,11 @@
 
   # age.identityPaths = ["/nix/persistent/etc/ssh/ssh_host_ed25519_key"];
 
-  #  fileSystems."/" = {
-  #    device = "tmpfs";
-  #    fsType = "tmpfs";
-  #    options = ["relatime" "mode=755" "nosuid" "nodev"];
-  #  };
+  fileSystems."/" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = ["relatime" "mode=755" "nosuid" "nodev"];
+  };
 
   # Impermanence will copy permissions from source dir
   # Chown to wf:wf
