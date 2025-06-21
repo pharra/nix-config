8554627e8354cb5e93f67124c16f94225d1bb126
pharra <typechasing@gmail.com>
2025-06-22 01:15:22 +0800

    hosts: add dat

--- hosts/dat/default.nix
+++ hosts/dat/default.nix
@@ -14,8 +14,6 @@
     ../../nixos/user-group.nix
   ];
 
-  # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
-  boot.binfmt.emulatedSystems = ["aarch64-linux" "riscv64-linux"];
   # supported fil systems, so we can mount any removable disks with these filesystems
   boot.supportedFilesystems = [
     "ext4"
@@ -38,15 +36,6 @@
     networkmanager.unmanaged = ["*,except:interface-name:wl*"];
   };
 
-  systemd.network = {
-    enable = true;
-    networks = {
-    };
-  };
-
-  # for Amd GPU
-  services.xserver.videoDrivers = ["nvidia"]; # will install nvidia-vaapi-driver by default
-
   hardware = {
     graphics = {
       enable = true;
@@ -54,7 +43,10 @@
     };
   };
 
-  wsl.enable = true;
+  wsl = {
+    enable = true;
+    defaultUser = "wf";
+  };
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
