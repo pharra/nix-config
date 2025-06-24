9efcd162ba9061caaa0e9c0b020ee1719a86b23f
pharra <typechasing@gmail.com>
2025-06-25 00:51:42 +0800

    flake: update nixos-wsl

--- flake.nix
+++ flake.nix
@@ -40,7 +40,10 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # nixos wsl
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # home-manager, used for managing user configuration
     home-manager = {
