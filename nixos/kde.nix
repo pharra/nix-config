2fa1381ef0fc716b97d54e4b2b6abfd362036192
pharra <typechasing@gmail.com>
2025-08-21 00:22:25 +0800

    nixos: update kde

--- nixos/kde.nix
+++ nixos/kde.nix
@@ -6,6 +6,7 @@
   programs = {
     zsh.enable = true;
     dconf.enable = true;
+    kdeconnect.enable = true;
   };
 
   services = {
@@ -30,6 +31,21 @@
   environment.systemPackages = with pkgs; [
     kdePackages.kirigami
     kdePackages.wallpaper-engine-plugin
+
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+    hardinfo2 # System information and benchmarks for Linux systems
+    haruna # Open source video player built with Qt/QML and libmpv
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
   ];
 
   i18n.inputMethod = {
