6091f1d126d0c0604f01325e899cf7381d5c86ce
pharra <typechasing@gmail.com>
2025-09-14 20:19:20 +0800

    nixos: fcitx: fix it in xwayland

--- home/input-method/fcitx5.nix
+++ home/input-method/fcitx5.nix
@@ -26,9 +26,8 @@ in {
   };
 
   programs.plasma = {
-    enable = true;
-    configFile = {
-      "kwinrc"."Wayland"."InputMethod[$e]" = "/run/current-system/sw/share/applications/org.fcitx.Fcitx5.desktop";
+    configFile.kwinrc = {
+      Wayland.InputMethod.value = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
     };
   };
 
