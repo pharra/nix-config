708149e037c0cb61dfd408c30f2d6f26ad50b34f
pharra <typechasing@gmail.com>
2025-10-23 02:45:23 +0800

    nixos: update cosmic

--- nixos/cosmic.nix
+++ nixos/cosmic.nix
@@ -7,25 +7,4 @@
 
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
-
-  environment = {
-    systemPackages = with pkgs; [
-    ];
-  };
-
-  security.polkit.extraConfig = ''
-    polkit.addRule(function(action, subject) {
-        if (action.id == "org.freedesktop.login1.suspend" ||
-            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
-            action.id == "org.freedesktop.login1.hibernate" ||
-            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
-        {
-            return polkit.Result.NO;
-        }
-    });
-  '';
-
-  # For gnome autologin issue, see https://github.com/NixOS/nixpkgs/issues/103746
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 }
