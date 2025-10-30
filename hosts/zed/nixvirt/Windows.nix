e6399c5d38b0ad7686dc5765b43321cf316a0952
pharra <typechasing@gmail.com>
2025-10-30 18:35:51 +0800

    nixos: zed: update windows vm

--- hosts/zed/nixvirt/Windows.nix
+++ hosts/zed/nixvirt/Windows.nix
@@ -215,7 +215,7 @@ in {
                 type = "pci";
                 mode = "subsystem";
                 managed = true;
-                source = {address = pci_address 2 0 0;};
+                source = {address = pci_address 4 0 0;};
                 # Intel SSD 760p 03:00.0
                 address = pci_address 8 0 0;
               }
