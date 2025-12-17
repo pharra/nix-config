897c7eb2d81d8e1cc702c4dbe7ba147a67a3a3f9
pharra <typechasing@gmail.com>
2025-12-17 18:21:11 +0800

    nixos: caddy: update lock

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -17,7 +17,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+      hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
     };
     configFile = config.age.secrets.caddy_homelab_conf.path;
   };
