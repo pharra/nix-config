6ccd91a64e11585dd5ad9a019036404905440aab
pharra <typechasing@gmail.com>
2026-02-15 16:47:29 +0800

    flake: update lock

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -27,7 +27,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-        hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
+        hash = "sha256-SrAHzXhaT3XO3jypulUvlVHq8oiLVYmH3ibh3W3aXAs=";
       };
       configFile = config.age.secrets.caddy_homelab_conf.path;
     };
