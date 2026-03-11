3f616de7fad1334ddb56d06cb7b893c2209850cc
pharra <typechasing@gmail.com>
2026-03-11 19:35:59 +0800

    hosts: zed: update Pat VM

--- hosts/zed/nixvirt/default.nix
+++ hosts/zed/nixvirt/default.nix
@@ -28,18 +28,33 @@ in {
   virtualisation.libvirtd.hooks.qemu."10-cpu-manager" = pkgs.writeShellScript "cpu-qemu-hook" ''
     machine=$1
     command=$2
+    log_tag="libvirt-hook"
+    log() {
+      ${pkgs.util-linux}/bin/logger -t "$log_tag" "$1"
+    }
+
+    log "qemu hook invoked: machine=$machine command=$command"
+
     if [ "$machine" == "Pat" ]; then
       if [ "$command" == "prepare" ]; then
-        ${pkgs.coreutils-full}/bin/echo "preparing"
+        log "Pat: preparing"
       elif [ "$command" == "started" ]; then
+        log "Pat: applying dedicated CPU mask (8-15,24-31)"
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- system.slice AllowedCPUs=8-15,24-31
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- user.slice AllowedCPUs=8-15,24-31
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- init.scope AllowedCPUs=8-15,24-31
+        log "Pat: dedicated CPU mask applied"
       elif [ "$command" == "stopped" ]; then
+        log "Pat: restoring full CPU mask (0-31)"
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- system.slice AllowedCPUs=0-31
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- user.slice AllowedCPUs=0-31
         ${pkgs.systemd}/bin/systemctl set-property --runtime -- init.scope AllowedCPUs=0-31
+        log "Pat: full CPU mask restored"
+      else
+        log "Pat: unhandled command=$command"
       fi
+    else
+      log "ignoring machine=$machine command=$command"
     fi
   '';
 }
