26bd9d4375b7cb3a3335c8db232950d84046586d
pharra <typechasing@gmail.com>
2026-04-13 13:49:07 +0800

    nixos: kde: remove sddm-kcm

--- modules/kde/default.nix
+++ modules/kde/default.nix
@@ -121,7 +121,6 @@ in {
       kdePackages.kcolorchooser # A small utility to select a color
       kdePackages.kolourpaint # Easy-to-use paint program
       kdePackages.ksystemlog # KDE SystemLog Application
-      kdePackages.sddm-kcm # Configuration module for SDDM
       kdiff3 # Compares and merges 2 or 3 files or directories
       kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
       kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
