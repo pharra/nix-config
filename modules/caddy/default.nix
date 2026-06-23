1afd53722661937a732c1c34903480fb6884057b
pharra <typechasing@gmail.com>
2026-06-23 21:42:08 +0800

    flake: update lock

--- modules/caddy/default.nix
+++ modules/caddy/default.nix
@@ -27,7 +27,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-        hash = "sha256-qEA6058svI8Q6yE97OkfnGWC8ayI3x8y2iU7PGkJ3Do=";
+        hash = "sha256-wHW0l15aLswe7gV9WioXo//abd0sJI82I7zIroRG3uU=";
       };
       configFile = config.age.secrets.caddy_homelab_conf.path;
     };
