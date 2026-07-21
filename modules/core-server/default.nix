695c75451db27cc08bd7f0e030693562c467a3b0
pharra <typechasing@gmail.com>
2026-07-21 11:30:37 +0800

    flake: update lock

--- modules/core-server/default.nix
+++ modules/core-server/default.nix
@@ -26,7 +26,7 @@ in {
 
     boot.binfmt.emulatedSystems = builtins.filter (p: p != pkgs.stdenv.hostPlatform.system) ["aarch64-linux" "x86_64-linux"];
 
-    boot.kernelPackages = lib.mkOverride 1400 pkgs.linuxPackages_7_0;
+    boot.kernelPackages = lib.mkOverride 1400 pkgs.linuxPackages_6_18;
 
     # supported fil systems, so we can mount any removable disks with these filesystems
     boot.supportedFilesystems = [
