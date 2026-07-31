4e6bd3e525b77c94067e1a69a893f422380ebaaa
pharra <typechasing@gmail.com>
2026-07-31 18:00:44 +0800

    overlay: remove linux-wallpaperengine overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,5 +2,5 @@
   # Add your overlays here
   # looking-glass-overlay = import ./looking-glass;
   # kwin-overlay = import ./kwin;
-  linux-wallpaperengine-overlay = import ./linux-wallpaperengine;
+  # linux-wallpaperengine-overlay = import ./linux-wallpaperengine;
 }
